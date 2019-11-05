21a0318736d35c5d5d24a55da61046aeab430db3
한나 <ordinaryhan@naver.com>
2019-11-05 21:33:13 +0900

    Revert "skybox, planet 모양 설정 + playerMove 수정 중"
    This reverts commit 170c2080daa60383b79961db523b06ca44a1b9fb.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public int playerSpeed;
+    bool grounded;
+    Rigidbody rigid;
+    public float jumpForce;
+    public LayerMask groundedMask;
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeed = 6;
+        jumpForce = 20;
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move
+        float inputY = Input.GetAxis("Vertical");
+        float inputX = Input.GetAxis("Horizontal");
+        Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+        Vector3 targetMoveAmount = moveDir * playerSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        //Jump
+        if (Input.GetKey(KeyCode.JoystickButton3)) //Y
+        {
+            if (grounded)
+            {
+                rigid.AddForce(transform.up * jumpForce);
+            }
+        }
+
+        // Grounded check
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1 + .1f, groundedMask))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        // Apply movement to rigidbody
+        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + localMove);
+    }
+
+}
