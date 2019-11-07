e611e0bfca5ca12d660c9db0319b8a5712074863
ji-water <bjisoo72@gmail.com>
2019-11-07 12:19:22 +0900

    HealthManager추가
    GameManager-> HealthManager 추가

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Heart = new GameObject[3];
+    private int hitCount;
+    
+    void Start()
+    {
+        hitCount = 0;
+        for(int i=0; i<3; i++)
+        {
+            Heart[i].SetActive(true);
+        }
+    }
+
+    public void Hit()
+    {
+        Heart[hitCount].SetActive(false);
+        hitCount++;
+    }
+
+}
