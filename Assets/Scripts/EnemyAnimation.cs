ef08e183a922df71f721d9559aaa913a1cb1e5a9
지현_Han <46097946+jhhan0317@users.noreply.github.com>
2019-11-15 21:48:55 +0900

    적 애니메이션

---
+++ Assets/Scripts/EnemyAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    
+    Animator ani;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // ani.SetInteger("animation", 15);
+    }
+}
---
+++ Assets/Scripts/ShotBehavior.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehavior : MonoBehaviour {
+
+    public GameObject explosion;
+    float distance;
+    Animator ani;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(destroyBullet());
+    }
+    // Update is called once per frame
+    void Update () {
+		transform.position += transform.forward * Time.deltaTime * 10f;
+	
+	}
+
+
+    IEnumerator destroyBullet()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Destroy(this.gameObject);
+
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            ani = other.gameObject.GetComponent<Animator>();
+            ani.SetInteger("animation", 6);
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+            Destroy(other.gameObject, 1.5f);
+            
+
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
