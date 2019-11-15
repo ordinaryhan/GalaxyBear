1e54b231035a2223d122ddfd1400997ad25f2ff6
지현_Han <46097946+jhhan0317@users.noreply.github.com>
2019-11-15 18:24:41 +0900

    메인씬 연결

--- Assets/VolumetricLines/ExampleScenes/ExampleScripts/ShotBehavior.cs
+++ Assets/VolumetricLines/ExampleScenes/ExampleScripts/ShotBehavior.cs
@@ -3,14 +3,41 @@ using System.Collections;
 
 public class ShotBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject explosion;
+    float distance;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(destroyBullet());
+    }
+    // Update is called once per frame
+    void Update () {
+		transform.position += transform.forward * Time.deltaTime * 10f;
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.position += transform.forward * Time.deltaTime * 1000f;
-	
-	}
+
+
+    IEnumerator destroyBullet()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Destroy(this.gameObject);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
 }
