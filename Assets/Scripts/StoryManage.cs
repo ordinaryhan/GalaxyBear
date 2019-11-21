4adff5f4ee28fbff8f2dd9613814df585ec8d002
ji-water <bjisoo72@gmail.com>
2019-11-21 12:18:59 +0900

    story 씬 추가, 메인 씬 수정
    5초마다 스토리 장면 자동으로 넘어가는 씬 추가 (씬 연결은 안함), 메인화면 raycast 경고문 수정 및 canvas 설정 변경

---
+++ Assets/Scripts/StoryManage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManage : MonoBehaviour
+{
+    int storyCnt = 0;
+    int storyNum = 3;
+    public GameObject[] story = new GameObject[3];
+
+    private IEnumerator coroutine;
+    
+    void Start()
+    {
+        story[1].SetActive(false);
+        story[2].SetActive(false);
+        coroutine = showStory();
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator showStory()
+    {
+        while (true)
+        {
+            storyCnt++;
+            if (storyCnt >= storyNum)
+                break;
+           
+            yield return new WaitForSeconds(5);
+
+            story[storyCnt].SetActive(true);
+            for(int i=0; i<storyNum; i++)
+            {
+                if (i != storyCnt)
+                    story[i].SetActive(false);
+            }
+        
+        }
+    }
+}
