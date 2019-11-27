300cba0b10f5bf40786888a4259cc6f23e4f3c3c
Jyw526 <55542627+Jyw526@users.noreply.github.com>
2019-11-27 22:28:05 +0900

    Spawner 임시
    gravity attractoer 오류 고쳐야

---
+++ Assets/JLBH_Scripts/Enemy.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    //public Transform player;
+    public float movePower = 1f;
+    int movementFlag = 0;
+
+    int MoveSpeed = 1;
+    int MaxDist = 5;
+    int MinDist = 4;
+
+    Vector3 position;
+
+
+    void Start()
+    {
+        position = (12) * Random.onUnitSphere;
+        //player = GameObject.Find("player2").GetComponent<Transform>();
+
+        StartCoroutine("ChangeMovement");
+        //nav = GetComponent<NavMeshAgent>();
+    }
+    //void Update()
+    //{
+    //    nav.SetDestination(player.position);
+    //}
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        
+        if (movementFlag==1)
+        {
+            moveVelocity = Vector3.left;
+            
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        else if (movementFlag == 2)
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (movementFlag == 3)
+        {
+            moveVelocity = Vector3.up;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (movementFlag == 4)
+        {
+            moveVelocity = Vector3.forward;
+        }
+
+        else if(movementFlag == 5)
+        {
+            moveVelocity = Vector3.one;
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+    void Update()
+
+    {
+        //transform.LookAt(position);
+    
+        //transform.LookAt((11) * Random.onUnitSphere);
+        //transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+        //transform.position = (11) * Random.onUnitSphere;
+        //transform.LookAt(player);
+
+        //if (Vector3.Distance(transform.position, player.position) >= MinDist)
+
+        //{
+
+        //    transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+        //    if (Vector3.Distance(transform.position, player.position) <= MaxDist)
+
+        //    {
+
+        //        //Here call any function you want Like Shoot at here or something
+
+        //    }
+        //    else
+
+        //    {
+
+        //        transform.position = (11) * Random.onUnitSphere;
+        //        Debug.Log("siiiiiiiiibaaaaaaaaaaaa");
+        //    }
+        }
+
+    IEnumerator ChangeMovement()
+    {
+        movementFlag = Random.Range(0, 6);
+        float time = Random.Range(2f, 5f);
+        yield return new WaitForSeconds(time);
+        StartCoroutine("ChangeMovement");
+    }
+    
+
+
+}
---
+++ Assets/JLBH_Scripts/SpawnManage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SpawnManage : MonoBehaviour
+{
+
+    private int MaxEnemy;
+    public int CurrentEnemy;
+    public GameObject Parent;
+    public GameObject[] Spawners = new GameObject[3];
+    public Transform EnemyPrefab;
+    Vector3 originscale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxEnemy = 20;
+        CurrentEnemy = 1;
+        StartCoroutine(Spawn());
+        //originscale = EnemyPrefab.transform.localScale;
+        Debug.Log(originscale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            if (CurrentEnemy < 20)
+            {
+                int ran = Random.Range(0, 3);
+                Debug.Log("Spawn from " + (ran+1));
+                Transform obj = GameObject.Instantiate(EnemyPrefab, Spawners[ran].transform.position, Quaternion.identity);
+                //obj.localScale = new Vector3(0.04f, 0.04f, 0.04f);
+                //obj.parent = Parent.transform;
+                obj.transform.SetParent(Parent.transform, true);
+
+                //obj.localScale = new Vector3(0.04f, 0.04f, 0.04f);
+
+                Debug.Log(obj.localScale);
+
+
+
+
+                CurrentEnemy++;
+                yield return new WaitForSeconds(5f);
+            }
+        }
+    }
+}
